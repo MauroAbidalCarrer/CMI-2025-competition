b6c80ab14b0d1ec7fad0d67b71ef841879231e4f
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-07-07 16:27:48 +0000

    Feat: Added new config constant variablas.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,13 @@
 COMPETITION_HANDLE = "cmi-detect-behavior-with-sensor-data"
+CATEGORY_COLUMNS = [
+    'row_id',
+    'sequence_type',
+    'sequence_id',
+    'subject',
+    'orientation',
+    'behavior',
+    'phase',
+    'gesture',
+]
+DATASET_DF_DTYPES = {col: "category" for col in CATEGORY_COLUMNS}
+SAMPLING_FREQUENCY = 10 #Hz
