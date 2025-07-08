8be90a82bd2d5214e3acbba7f90d4bc375513fd7
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2025-07-08 18:36:52 +0200

    Feat: Added a few constants in config.py.

--- src/config.py
+++ src/config.py
@@ -1,8 +1,20 @@
 COMPETITION_HANDLE = "cmi-detect-behavior-with-sensor-data"
+# Dataset
 CATEGORY_COLUMNS = [
     'row_id',
     'sequence_type',
     'sequence_id',
+    'subject',
+    'orientation',
+    'behavior',
+    'phase',
+    'gesture',
+]
+META_DATA_COLUMNS = [
+    'row_id',
+    'sequence_type',
+    'sequence_id',
+    'sequence_counter',
     'subject',
     'orientation',
     'behavior',
