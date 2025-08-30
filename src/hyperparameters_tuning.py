f135a89485779dfd6f50e2578367e7ff3c6b7ebb
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-08-30 15:12:38 +0000

    Feat: Updated hyper parameter tuning script to use the val dation split score to compute the final score intead of the mean of the CV score.

--- src/hyperparameters_tuning.py
+++ src/hyperparameters_tuning.py
@@ -1,10 +1,18 @@
+from functools import partial
+
+import torch
 import optuna
+from pandas import DataFrame as DF
 from optuna.trial import TrialState
 from optuna.pruners import BasePruner
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader as DL
 
 from config import *
-
-from training import train_on_all_folds
+from model import mk_model_ensemble
+from preprocessing import get_meta_data
+from dataset import split_dataset, get_fold_datasets, move_cmi_dataset
+from training import train_on_all_folds, evaluate_model
 
 class FoldPruner(BasePruner):
     def __init__(
@@ -32,8 +40,16 @@ class FoldPruner(BasePruner):
         
         return current_val < (ref - self.tolerance)
 
-def objective(trial: optuna.trial.Trial) -> float:
-    return train_on_all_folds(
+def objective(
+        trial: optuna.trial.Trial,
+        train_datasets: list[TensorDataset],
+        train_seq_meta:DF,
+        preprocessed_meta_data: dict,
+        val_loader: DL,
+        val_device: torch.device,
+    ) -> float:
+    train_on_all_folds(
+        train_datasets,
         # lr_scheduler_kw={
         #     "warmup_epochs": trial.suggest_int("warmup_epochs", 12, 15),
         #     "cycle_mult": trial.suggest_float("cycle_mult", 0.9, 1.6, step=0.1),
@@ -79,12 +95,29 @@ def objective(trial: optuna.trial.Trial) -> float:
             "elbow_to_wrist_ratio_loss_weight": trial.suggest_float("limbs_length_loss_weight", 0, 0.6, step=0.1),
             "shoulder_to_elbow_ratio_loss_weight": trial.suggest_float("limbs_length_loss_weight", 0, 0.6, step=0.1),
         },
-        trial=trial,
-    )[0]
+        seq_meta=train_seq_meta,
+    )
+    ensemble = mk_model_ensemble("models", val_device)
+    val_metrics = evaluate_model(preprocessed_meta_data, ensemble, val_loader, torch.nn.CrossEntropyLoss(), val_device)
+    return val_metrics["final_metric"]
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize", pruner=FoldPruner(warmup_steps=0, tolerance=0.002))
-    study.optimize(objective, n_trials=100, timeout=60 * 60 * 8)
+    splits = split_dataset()
+    val_device = torch.device("cuda")
+    train_datasets = get_fold_datasets(splits["train"][0])
+    val_dataset = move_cmi_dataset(splits["validation"][0], val_device)
+    val_loader = DL(val_dataset, VALIDATION_BATCH_SIZE, shuffle=True)
+    preprocessed_meta_data = get_meta_data()
+    part_objective = partial(
+        objective,
+        train_datasets=train_datasets,
+        train_seq_meta=splits["train"][1],
+        val_loader=val_loader,
+        preprocessed_meta_data=preprocessed_meta_data,
+        val_device=val_device,
+    )
+    study.optimize(part_objective, n_trials=100, timeout=60 * 60 * 8)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
